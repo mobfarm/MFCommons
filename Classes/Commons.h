6bb14fe2a39214fa42fa645f235f31194ad35618
nick <nick@mobfarm.eu>
2013-09-16 17:49:48 +0200

    Removed dependancy from MFLog

---
+++ Classes/Commons.h
@@ -0,0 +1,284 @@
+//
+//  Commons.h
+//  CustomContainerViewControllerTest
+//
+//  Created by Nicolò Tosi on 6/6/13.
+//  Copyright (c) 2013 MobFarm. All rights reserved.
+//
+
+#ifndef Commons_h
+#define Commons_h
+
+#import <CoreGraphics/CGGeometry.h>
+
+#define TOLERANCE 0.1 // !!!
+
+#define IS_ZERO(x) (ABS((x)) < FLT_EPSILON)
+
+static BOOL lineIntersection(CGPoint p0, CGPoint p1, CGPoint p2, CGPoint p3, CGPoint * where);
+static CGPoint lineSecondPoint(CGPoint p0, float angle);
+static CGFloat crossProduct2D(CGPoint p0, CGPoint p1);
+static CGFloat dotProduct2D(CGPoint p0, CGPoint p1);
+static BOOL boxLineIntersection(CGPoint l0, CGPoint l1, CGPoint b0, CGPoint b1, CGPoint b2, CGPoint b3, CGPoint * x0, CGPoint * x1);
+static BOOL liesOnSegment(CGPoint p0, CGPoint s0, CGPoint s1);
+static CGFloat pointToPointDistance(CGPoint p0, CGPoint p1);
+static BOOL pointCoincideToPoint(CGPoint p0, CGPoint p1);
+static CGPoint subtractPointToPoint(CGPoint p0, CGPoint p1);
+
+#ifndef ZEROED_RECT
+#define ZEROED_RECT(x) CGRectMake(0,0,(x).size.width,(x).size.height)
+#endif
+
+#ifndef RECT_POS
+#define RECT_POS(z) CGPointMake((z).origin.x + (z).size.width * 0.5, (z).origin.y + (z).size.height * 0.5)
+#endif
+
+static CGRect rectWithSize(CGSize size)
+{
+    return CGRectMake(0, 0, size.width, size.height);
+}
+
+static float clip(float v, float min, float max)
+{
+	return	MAX(min,MIN(v,max));
+}
+
+static float circumscribedRadius(CGRect rect)
+{
+    CGFloat halfWidth = rect.size.width/2;
+    CGFloat halfWidthSquared = halfWidth * halfWidth;
+    CGFloat halfHeight = rect.size.height/2;
+    CGFloat halfHeightSquared = halfHeight * halfHeight;
+    return sqrtf(halfHeightSquared + halfWidthSquared);
+}
+
+static CGPoint shiftVector(CGFloat distance, float angle)
+{
+    CGFloat dx, dy;
+    
+    dx = ceilf(cosf(angle) * distance);
+    dy = ceilf(sinf(angle) * distance);
+    
+    return CGPointMake(dx, dy);
+}
+
+static CGPoint shiftCenter(CGPoint start, CGFloat distance, float angle)
+{
+    CGPoint shift = shiftVector(distance, angle);
+    return CGPointMake(start.x + shift.x, start.y + shift.y);
+}
+
+static CGPoint translationVector(CGRect rect, float angle)
+{
+    CGFloat dx, dy;
+    CGFloat containerRadius = circumscribedRadius(rect);
+    
+    dx = clip(containerRadius * cosf(angle), -rect.size.width/2, rect.size.width/2);
+    dy = clip(containerRadius * sinf(angle), -rect.size.height/2, rect.size.height/2);
+    
+    return CGPointMake(dx, dy);
+}
+
+static CGFloat pointToPointDistance(CGPoint p0, CGPoint p1)
+{
+    CGFloat deltaX = p0.x - p1.x;
+    CGFloat deltaY = p0.y - p1.y;
+    return sqrtf(deltaX * deltaX + deltaY * deltaY);
+}
+
+static BOOL boxLineIntersection(CGPoint l0, CGPoint l1, CGPoint b0, CGPoint b1, CGPoint b2, CGPoint b3, CGPoint * x0, CGPoint * x1)
+{
+    CGPoint firstIP, secondIP;
+    BOOL firstIPfound = NO;
+    BOOL secondIPfound = NO;
+    
+    CGPoint p = CGPointMake(FLT_MAX, FLT_MAX);
+    
+    if(lineIntersection(l0, l1, b0, b1, &p))
+    {
+        // NSLog(@"found %@", NSStringFromCGPoint(p));
+        if(liesOnSegment(p, b0, b1))
+        {
+            firstIP = p, firstIPfound = YES;
+        }
+    }
+    
+    if(lineIntersection(l0, l1, b1, b2, &p))
+    {
+        // NSLog(@"found %@", NSStringFromCGPoint(p));
+        if(liesOnSegment(p, b1, b2))
+        {
+            if(firstIPfound)
+            {
+                if(!pointCoincideToPoint(firstIP, p))
+                {
+                    secondIP = p, secondIPfound = YES;
+                }
+            }
+            else
+            {
+                firstIP = p, firstIPfound = YES;
+            }
+        }
+    }
+    
+    if(lineIntersection(l0, l1, b2, b3, &p))
+    {
+        // NSLog(@"found %@", NSStringFromCGPoint(p));
+        if(liesOnSegment(p, b2, b3))
+        {
+            if(firstIPfound)
+            {
+                if(!pointCoincideToPoint(firstIP, p))
+                {
+                    secondIP = p, secondIPfound = YES;
+                }
+            }
+            else
+            {
+                firstIP = p, firstIPfound = YES;
+            }
+        }
+    }
+    
+    if(lineIntersection(l0, l1, b3, b0, &p))
+    {
+        // NSLog(@"found %@", NSStringFromCGPoint(p));
+        if(liesOnSegment(p, b3, b0))
+        {
+            if(firstIPfound)
+            {
+                if(!pointCoincideToPoint(firstIP, p))
+                {
+                    secondIP = p, secondIPfound = YES;
+                }
+            }
+            else
+            {
+                firstIP = p, firstIPfound = YES;
+            }
+        }
+    }
+    
+    if(secondIPfound)
+    {
+        
+        *x0 = firstIP;
+        *x1 = secondIP;
+        return YES;
+    }
+    else if (firstIPfound)
+    {
+        *x0 = *x1 = firstIP;
+        return YES;
+    }
+    
+    return NO;
+}
+
+static BOOL liesOnSegment(CGPoint p0, CGPoint s0, CGPoint s1)
+{
+    CGFloat deltaS = pointToPointDistance(s0, s1);
+    CGFloat deltaPS0 = pointToPointDistance(p0, s0);
+    CGFloat deltaPS1 = pointToPointDistance(p0, s1);
+    
+    if(fabsf(deltaS - (deltaPS0 + deltaPS1)) < TOLERANCE)
+        return YES;
+    
+    return NO;
+}
+
+static CGPoint subtractPointToPoint(CGPoint p0, CGPoint p1)
+{
+    return CGPointMake(p1.x - p0.x, p1.y - p0.y);
+}
+
+static float dotProduct(CGPoint p0, CGPoint p1)
+{
+    return p0.x * p1.x + p0.y * p1.y;
+}
+
+static CGFloat crossProduct2D(CGPoint p0, CGPoint p1)
+{
+    return p0.x * p1.y - p0.y * p1.x;
+}
+
+static CGPoint lineSecondPoint(CGPoint p0, float angle)
+{
+    CGFloat dx = cosf(angle);
+    CGFloat dy = sinf(angle);
+    
+    return CGPointMake(p0.x + dx, p0.y + dy);
+}
+
+static BOOL pointCoincideToPoint(CGPoint p0, CGPoint p1)
+{
+    return (fabs(pointToPointDistance(p0, p1)) < TOLERANCE);
+}
+
+static BOOL lineIntersection(CGPoint p0, CGPoint p1, CGPoint p2, CGPoint p3, CGPoint * where)
+{
+    float det = (p0.x - p1.x) * (p2.y - p3.y) - (p0.y - p1.y) * (p2.x - p3.x);
+    
+    if(IS_ZERO(det))
+        return NO;
+    
+	float x = ((p2.x - p3.x) * (p0.x * p1.y - p0.y * p1.x) - (p0.x - p1.x) * (p2.x * p3.y - p2.y * p3.x))/det;
+	float y = ((p2.y - p3.y) * (p0.x * p1.y - p0.y * p1.x) - (p0.y - p1.y) * (p2.x * p3.y - p2.y * p3.x))/det;
+	
+    *where = CGPointMake(x, y);
+    
+	return YES;
+}
+
+@interface Commons : NSObject
+
++(NSString *)randomIdentifier;
+
++(void)checksumOfFile:(NSString *)file hex:(unsigned char *)hexes;
++(NSString *)checksumStringFromChecksumByteArray:(unsigned char *)checksum;
++(void)convertChecksumString:(NSString *)string toByteArray:(unsigned char *)hexes;
++(BOOL)isFile:(NSString *)file0
+   equalToFile:(NSString *)file1;
+
++(NSString *)MACAddress;
++(NSString *)filesystemSafeMACAddress;
+
+/* Update server */
++(void)setServerAddress:(NSString *)address;
++(NSString *)serverAddress;
++(NSString *)httpServerAddress;
+
+/* Database preferences */
++(BOOL)useDevelopmentDatabase;
++(void)setUseDevelopmentDatabase:(BOOL)useOrNot;
++(BOOL)useReadWriteDatabase;
++(void)setUseReadWriteDatabase:(BOOL)useRWorNot;
+
+/* Saved session ID */
++(void)setCurrentSessionID:(NSString *)sessionID;
++(NSString *)currentSessionID;
+
+/* Upload server */
++(NSString *)uploadServerAddress;
++(void)setUploadServerAddress:(NSString *)address;
++(NSUInteger)uploadServerPort;
++(void)setUploadServerPort:(NSUInteger)port;
+
++(NSDateFormatter *)defaultDateFormatter;
++(NSDateFormatter *)filesystemSafeDateFormatter;
+
++(NSString *)databaseUploadDirectory;
++(void)setDatabaseUploadDirectory:(NSString *)directory;
+
++(NSURL *)URLForApplicationLibraryDirectory;
++(NSString *)filePathForApplicationLibraryDirectory;
++(NSString *)filePathForApplicationDocumentsDirectory;
++(NSURL *)URLForApplicationDocumentsDirectory;
++(NSString *)filePathForApplicationTemporaryDirectory;
++(NSString *)filePathForApplicationCachesDirectory;
+
+
+@end
+
+#endif
---
+++ Classes/Commons.m
@@ -0,0 +1,430 @@
+//
+//  Commons.m
+//  eXplora MuSe
+//
+//  Created by Nicolò Tosi on 6/22/13.
+//  Copyright (c) 2013 MobFarm. All rights reserved.
+//
+
+#import "Commons.h"
+#import <CommonCrypto/CommonCrypto.h>
+
+@implementation Commons
+
++(NSString *)randomIdentifier
+{
+    int randoms [8];
+    
+    for(int i = 0; i < 8; i++)
+    {
+        randoms[i] = arc4random();
+    }
+    
+    return [NSString stringWithFormat:@"%08x%08x%08x%08x%08x%08x%08x%08x",randoms[0],randoms[1],randoms[2],randoms[3],randoms[4],randoms[5],randoms[6],randoms[7]];
+}
+
+
+
++(NSString *)checksumStringFromChecksumByteArray:(unsigned char *)checksum
+{
+    return [NSString stringWithFormat:@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+            checksum[0], checksum[1], checksum[2], checksum[3], checksum[4],checksum[5],checksum[6],checksum[7],
+            checksum[8], checksum[9], checksum[10], checksum[11], checksum[12], checksum[13], checksum[14],checksum[15]];
+}
+
+static char hexConversionTable [] = {
+    0,1,2,3,4,5,6,7,8,9,
+    -1,-1,-1,-1,-1,-1,-1,
+    10,11,12,13,14,15,
+    -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+        10,11,12,13,14,15
+};
+
++(void)convertChecksumString:(NSString *)string toByteArray:(unsigned char *)hexes
+{
+    NSUInteger stringLength = string.length;
+    
+    if(stringLength%2 != 0)
+        return;
+    
+    for (NSUInteger count = 0; count < string.length; count+=2) {
+        
+        unichar firstByte = [string characterAtIndex:count];
+        unichar secondByte = [string characterAtIndex:count + 1];
+        
+        if(firstByte < '0' || firstByte > 'f' || secondByte < '0' || secondByte > 'f')
+            return;
+        
+        int firstByteValue = hexConversionTable[firstByte - '0'];
+        int secondByteValue = hexConversionTable[secondByte - '0'];
+        
+        if(firstByteValue >= 0 && secondByteValue >= 0)
+        {
+            hexes[count/2] = (firstByteValue * 16) + secondByteValue;
+        }
+        else
+        {
+            return;
+        }   
+    }
+}
+
++(void)checksumOfFile:(NSString *)file hex:(unsigned char *)hexes
+{
+    CC_MD5_CTX * ctx = malloc(sizeof(CC_MD5_CTX));
+    CC_MD5_Init(ctx);
+    
+    NSData * data = [NSData dataWithContentsOfFile:file];
+    
+    CC_MD5_Update(ctx, [data bytes], [data length]);
+    
+    CC_MD5_Final(hexes, ctx);
+    
+    free(ctx);
+}
+
++(BOOL)isFile:(NSString *)file0
+   equalToFile:(NSString *)file1
+{
+    unsigned char digest0[CC_MD5_DIGEST_LENGTH];
+    unsigned char digest1[CC_MD5_DIGEST_LENGTH];
+    
+    [Commons checksumOfFile:file0 hex:digest0];
+    [Commons checksumOfFile:file1 hex:digest1];
+    
+    return (memcmp(digest0, digest1, CC_MD5_DIGEST_LENGTH) == 0);
+}
+
+#include <sys/socket.h>
+#include <sys/sysctl.h>
+#include <net/if.h>
+#include <net/if_dl.h>
+
+/* Settings keys */
+static NSString * MFExploraServerAddressKey = @"MFExploraServerAddress";
+static NSString * MFExploraUseDevelopmentDatabaseKey = @"MFExploraUseDevelopmentDatabase";
+static NSString * MFExploraUseRWDatabaseKey = @"MFexploraUseRWDatabase";
+static NSString * MFExploraCurrentSessionIDKey = @"MFExploraCurrentSessionID";
+static NSString * MFExploraUploadServerAddressKey = @"MFExploraUploadServerAddress";
+static NSString * MFExploraUploadServerPortKey = @"MFExploraUploadServerPort";
+static NSString * MFExploraUploadDatabaseDirectoryKey = @"MFExploraUploadDatabaseDirectoryKey";
+
+/* Settings fallback values */
+static NSString * MFExploraDefaultServerAddress = @"bundles.mobfarm.eu";
+static NSString * MFExploraDefaultUploadServerAddress = @"mobfarm.eu";
+static NSUInteger MFExploraDefaultUploadServerPort = 17345;
+static NSString * MFExploraDefaultUploadDatabaseDirectory = @"DatabaseUpdates";
+
+#pragma mark - Settings for server address
+
++(NSString *)serverAddress
+{
+    NSString * address = [[NSUserDefaults standardUserDefaults]valueForKey:MFExploraServerAddressKey];
+    
+    if(address.length <= 0)
+    {
+        address = MFExploraDefaultServerAddress;
+    }
+    
+    return address;
+}
+
++(NSString *)httpServerAddress
+{
+    NSString * address = [Commons serverAddress];
+    
+    if([address hasPrefix:@"http"])
+    {
+        return address;
+    }
+    else
+    {
+        return [@"http://" stringByAppendingString:address];
+    }
+}
+
++(void)setServerAddress:(NSString *)address
+{
+    if(address.length > 0)
+    {
+        [[NSUserDefaults standardUserDefaults]setValue:address
+                                                forKey:MFExploraServerAddressKey];
+    }
+    else
+    {
+        [[NSUserDefaults standardUserDefaults]removeObjectForKey:MFExploraServerAddressKey];
+    }
+    
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+#pragma mark - Settings for upload server
+
++(NSString *)uploadServerAddress
+{
+    NSString * address = [[NSUserDefaults standardUserDefaults] valueForKey:MFExploraUploadServerAddressKey];
+    
+    if(address.length <= 0)
+    {
+        address = MFExploraDefaultUploadServerAddress;
+    }
+    
+    return address;
+}
+
++(void)setUploadServerAddress:(NSString *)address;
+{
+    if(address.length > 0)
+    {
+        [[NSUserDefaults standardUserDefaults]setValue:address
+                                                forKey:MFExploraUploadServerAddressKey];
+    }
+    else
+    {
+        [[NSUserDefaults standardUserDefaults]removeObjectForKey:MFExploraUploadServerAddressKey];
+    }
+    [[NSUserDefaults standardUserDefaults]synchronize];
+}
+
++(NSUInteger)uploadServerPort
+{
+    NSUInteger port = [[[NSUserDefaults standardUserDefaults]valueForKey:MFExploraUploadServerPortKey]unsignedIntegerValue];
+    
+    if(port <= 0)
+    {
+        port = MFExploraDefaultUploadServerPort;
+    }
+    
+    return port;
+}
+
++(void)setUploadServerPort:(NSUInteger)port
+{
+    if(port > 0)
+    {
+        [[NSUserDefaults standardUserDefaults]setValue:[NSNumber numberWithUnsignedInteger:port]
+                                                forKey:MFExploraUploadServerPortKey];
+    }
+    else
+    {
+        [[NSUserDefaults standardUserDefaults]removeObjectForKey:MFExploraUploadServerPortKey];
+    }
+    
+    [[NSUserDefaults standardUserDefaults]synchronize];
+}
+
+
++(NSString *)databaseUploadDirectory
+{
+    NSString * directory = [[NSUserDefaults standardUserDefaults] valueForKey:MFExploraUploadDatabaseDirectoryKey];
+    
+    if(directory.length <= 0)
+    {
+        directory = MFExploraDefaultUploadDatabaseDirectory;
+    }
+    
+    return directory;
+}
+
++(void)setDatabaseUploadDirectory:(NSString *)directory
+{
+    if(directory.length > 0)
+    {
+        [[NSUserDefaults standardUserDefaults]setValue:directory
+                                                forKey:MFExploraUploadDatabaseDirectoryKey];
+    }
+    else
+    {
+        [[NSUserDefaults standardUserDefaults]removeObjectForKey:MFExploraUploadDatabaseDirectoryKey];
+    }
+    [[NSUserDefaults standardUserDefaults]synchronize];
+}
+
+#pragma mark - Date formatters
+
++(NSDateFormatter *)filesystemSafeDateFormatter
+{
+    static NSDateFormatter * formatter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSDateFormatter* dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setDateFormat:@"yyyy-MM-dd_HH'h'mm'm'ss's'"];
+        formatter = dateFormatter;
+    });
+    return formatter;
+}
+
++(NSDateFormatter *)defaultDateFormatter
+{
+    static NSDateFormatter * formatter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSDateFormatter* dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+        formatter = dateFormatter;
+    });
+    return formatter;
+}
+
+#pragma mark - Paths and URLs
+
++(NSURL *)URLForApplicationLibraryDirectory
+{
+    return [[[NSFileManager defaultManager]URLsForDirectory:NSLibraryDirectory inDomains:NSUserDomainMask]lastObject];
+}
+
++(NSString *)filePathForApplicationLibraryDirectory
+{
+    return [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) lastObject];
+}
+
++(NSString *)filePathForApplicationDocumentsDirectory
+{
+    return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+}
+
++(NSString *)filePathForApplicationTemporaryDirectory
+{
+    return NSTemporaryDirectory();
+}
+
++(NSURL *)URLForApplicationDocumentsDirectory
+{
+    return [[[NSFileManager defaultManager]URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask]lastObject];
+}
+
++(NSString *)filePathForApplicationCachesDirectory
+{
+    return [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject];
+}
+
++(NSString *)URLForApplicationCacheDirectory
+{
+    return [NSURL fileURLWithPath:[Commons filePathForApplicationCachesDirectory]];
+}
+
+#pragma mark - Settings for DB
+
++(BOOL)useReadWriteDatabase
+{
+    return [[[NSUserDefaults standardUserDefaults]valueForKey:MFExploraUseRWDatabaseKey]boolValue];
+}
+
++(void)setUseReadWriteDatabase:(BOOL)useRWorNot
+{
+    [[NSUserDefaults standardUserDefaults]setValue:[NSNumber numberWithBool:useRWorNot] forKey:MFExploraUseRWDatabaseKey];
+    [[NSUserDefaults standardUserDefaults]synchronize];
+}
+
++(BOOL)useDevelopmentDatabase
+{
+    return [[[NSUserDefaults standardUserDefaults]valueForKey:MFExploraUseDevelopmentDatabaseKey]boolValue];
+}
+
++(void)setUseDevelopmentDatabase:(BOOL)useOrNot
+{
+    [[NSUserDefaults standardUserDefaults]setValue:[NSNumber numberWithBool:useOrNot] forKey:MFExploraUseDevelopmentDatabaseKey];
+    [[NSUserDefaults standardUserDefaults]synchronize];
+}
+
+
+#pragma mark - Settings for saved session
+
++(NSString *)currentSessionID
+{
+    return [[NSUserDefaults standardUserDefaults]valueForKey:MFExploraCurrentSessionIDKey];
+}
+
++(void)setCurrentSessionID:(NSString *)sessionID
+{
+    if(sessionID.length > 0)
+    {
+        [[NSUserDefaults standardUserDefaults]setValue:sessionID forKey:MFExploraCurrentSessionIDKey];
+    }
+    else
+    {
+        [[NSUserDefaults standardUserDefaults]removeObjectForKey:MFExploraCurrentSessionIDKey];
+    }
+    
+    [[NSUserDefaults standardUserDefaults]synchronize];
+}
+
+#pragma mark - Others
+
++(NSString *)filesystemSafeMACAddress
+{
+    NSString * MACAddress = [Commons MACAddress];
+    
+    return [MACAddress stringByReplacingOccurrencesOfString:@":" withString:@""];
+}
+
++(NSString *)MACAddress
+{
+    int                 mgmtInfoBase[6];
+    char                *msgBuffer = NULL;
+    size_t              length;
+    unsigned char       macAddress[6];
+    struct if_msghdr    *interfaceMsgStruct;
+    struct sockaddr_dl  *socketStruct;
+    NSString            *errorFlag = NULL;
+    
+    // Setup the management Information Base (mib)
+    mgmtInfoBase[0] = CTL_NET;        // Request network subsystem
+    mgmtInfoBase[1] = AF_ROUTE;       // Routing table info
+    mgmtInfoBase[2] = 0;
+    mgmtInfoBase[3] = AF_LINK;        // Request link layer information
+    mgmtInfoBase[4] = NET_RT_IFLIST;  // Request all configured interfaces
+    
+    // With all configured interfaces requested, get handle index
+    if ((mgmtInfoBase[5] = if_nametoindex("en0")) == 0)
+        errorFlag = @"if_nametoindex failure";
+    else
+    {
+        // Get the size of the data available (store in len)
+        if (sysctl(mgmtInfoBase, 6, NULL, &length, NULL, 0) < 0)
+            errorFlag = @"sysctl mgmtInfoBase failure";
+        else
+        {
+            // Alloc memory based on above call
+            if ((msgBuffer = malloc(length)) == NULL)
+                errorFlag = @"buffer allocation failure";
+            else
+            {
+                // Get system information, store in buffer
+                if (sysctl(mgmtInfoBase, 6, msgBuffer, &length, NULL, 0) < 0)
+                    errorFlag = @"sysctl msgBuffer failure";
+            }
+        }
+    }
+    
+    // Befor going any further...
+    if (errorFlag != NULL)
+    {
+        NSLog(@"Error: %@", errorFlag);
+        return errorFlag;
+    }
+    
+    // Map msgbuffer to interface message structure
+    interfaceMsgStruct = (struct if_msghdr *) msgBuffer;
+    
+    // Map to link-level socket structure
+    socketStruct = (struct sockaddr_dl *) (interfaceMsgStruct + 1);
+    
+    // Copy link layer address data in socket structure to an array
+    memcpy(&macAddress, socketStruct->sdl_data + socketStruct->sdl_nlen, 6);
+    
+    // Read from char array into a string object, into traditional Mac address format
+    NSString *macAddressString = [NSString stringWithFormat:@"%02X:%02X:%02X:%02X:%02X:%02X",
+                                  macAddress[0], macAddress[1], macAddress[2],
+                                  macAddress[3], macAddress[4], macAddress[5]];
+    //MFLogError(@"Mac Address: %@", macAddressString);
+    
+    // Release the buffer memory
+    free(msgBuffer);
+    
+    return macAddressString;
+}
+
+
+@end
